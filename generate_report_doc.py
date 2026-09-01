ce462415f54564886473d79264ad4fbbee9645f1
chaitanyakumarAI <rangan@iitbhilai.ac.in>
2026-09-01 22:17:23 +0530

    docs: update live API URL to http://10.1.75.51:5237/analyzeContour (confirmed working)

--- generate_report_doc.py
+++ generate_report_doc.py
@@ -129,8 +129,8 @@ def create_report():
     add_body("Ranga Chandra Naga Venkata Chaitanya Kumar", "Student Name: ")
     add_body("12341740", "Roll Number: ")
     add_body("https://github.com/chaitanyakumarAI/Pond_catchment_analysis.git", "GitHub Repository Link: ")
-    add_body("http://10.1.75.51:5050/analyzeContour (or http://10.1.75.51:5000/analyzeContour)", "Working API Route URL: ")
-    add_body("http://10.1.75.51:5050/", "Live Server Base URL: ")
+    add_body("http://10.1.75.51:5237/analyzeContour (and http://10.1.75.51:5237/findCatchment)", "Working Live API Route URL: ")
+    add_body("http://10.1.75.51:5237/", "Live Dashboard URL: ")
 
     # 2. Catchment Estimation Approach
     add_heading_1("2. Terrain Analysis & Catchment Estimation Approach")
